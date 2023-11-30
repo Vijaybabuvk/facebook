caa55ca2ebf54e2fb879744825a4e25113a06278
raavangokul007 <raavangokuldd@gmail.com>
2023-11-30 01:01:57 +0000

    updating chags

--- depjava.sh
+++ depjava.sh
@@ -1,2 +1,3 @@
 This is the first line of this file..!
+This is the check for git stash..!`
 This is second line of the file...!
--- deployment.sh
+++ deployment.sh
@@ -3,3 +3,6 @@
 echo "This the file going to be deploy the infra in aws and azure"
 
 echo "Thanks..!"
+
+
+echo "testing the git stash"
